52cfb3af40b4ede9fd9f86c98d3d99769710f1f3
marcelokscunha <marcelocunha92@hotmail.com>
2018-01-21 03:55:47 -0200

    shebang

--- client/scripts/appendApacheConfig.sh
+++ client/scripts/appendApacheConfig.sh
@@ -1,3 +1,5 @@
+  #!/bin/bash
+  
   cat /etc/httpd/conf/httpd.conf /var/www/html/apacheConfig.conf > /etc/httpd/conf/result.conf
   mv /etc/httpd/conf/result.conf /etc/httpd/conf/httpd.conf
   rm /etc/httpd/conf/apacheConfig.conf

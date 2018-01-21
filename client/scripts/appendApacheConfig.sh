d3486772bf01647ef10997120ff9cedcc12bdd75
marcelokscunha <marcelocunha92@hotmail.com>
2018-01-21 03:33:56 -0200

    apache config

--- client/scripts/appendApacheConfig.sh
+++ client/scripts/appendApacheConfig.sh
@@ -1,4 +1,6 @@
   cat /etc/httpd/conf/httpd.conf /var/www/html/apacheConfig.conf > /etc/httpd/conf/result.conf
   mv /etc/httpd/conf/result.conf /etc/httpd/conf/httpd.conf
   rm /etc/httpd/conf/apacheConfig.conf
+
+  service httpd restart
   

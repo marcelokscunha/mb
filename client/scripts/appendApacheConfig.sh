b571206d7585b9826551735bb10711a13dd53615
marcelokscunha <marcelocunha92@hotmail.com>
2018-01-22 18:40:49 -0200

    + buildspec

--- client/scripts/appendApacheConfig.sh
+++ client/scripts/appendApacheConfig.sh
@@ -1,9 +1,7 @@
   #!/bin/bash
 
-  echo "roda"
   cat /etc/httpd/conf/httpd.conf /var/www/html/apacheConfig.conf > /etc/httpd/conf/result.conf
   mv /etc/httpd/conf/result.conf /etc/httpd/conf/httpd.conf
-  echo "ALOOWWWWW"
   rm /var/www/html/apacheConfig.conf
 
   service httpd restart

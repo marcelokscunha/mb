ef6500fd69cd5863a8624db1dacd7ab564064d95
marcelokscunha <marcelocunha92@hotmail.com>
2018-01-23 01:31:40 -0200

    no sudo's LoadTest.sh

--- backend/load-test/LoadTest.sh
+++ backend/load-test/LoadTest.sh
@@ -8,33 +8,33 @@ pwd
 echo "0 -  "
 ls
 
-sudo mv $SCRIPT_PATH/LoadTest.scala $MYPATH/user-files/simulations
+mv $SCRIPT_PATH/LoadTest.scala $MYPATH/user-files/simulations
 pwd
 echo "1 -  "
 ls
 
-sudo $MYPATH/bin/gatling.sh -s LoadTest
+$MYPATH/bin/gatling.sh -s LoadTest
 pwd
 echo "2 -  "
 ls
 
-sudo cd $MYPATH/results
+cd $MYPATH/results
 pwd
 echo "3 -  "
 ls
 
-sudo aws s3 sync $MYPATH/results s3://mb-gatling-reports/reports
+aws s3 sync $MYPATH/results s3://mb-gatling-reports/reports
 # aws s3 cp ./* s3://mb-gatling-reports/reports --recursive
 
 pwd
 echo "4 -  "
 ls
-sudo cd $MYPATH/results/*
+cd $MYPATH/results/*
 
 echo "5 -  "
 pwd
 ls
 aws s3 rm s3://mb-gatling-reports --recursive --exclude "reports/*"
-sudo aws s3 sync $MYPATH/results/* s3://mb-gatling-reports
+aws s3 sync $MYPATH/results/* s3://mb-gatling-reports
 
-sudo rm -r $MYPATH/results/*
+rm -r $MYPATH/results/*
